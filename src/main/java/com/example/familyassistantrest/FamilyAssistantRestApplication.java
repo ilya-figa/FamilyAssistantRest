d70741f4803c1db34e6b861e194470f17509538f
Ilya Buyevich <ibuyevich@mdait.by>
2025-04-03 13:14:31 +0300

    init project

---
+++ src/main/java/com/example/familyassistantrest/FamilyAssistantRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.familyassistantrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyAssistantRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyAssistantRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/familyassistantrest/FamilyAssistantRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.familyassistantrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyAssistantRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
