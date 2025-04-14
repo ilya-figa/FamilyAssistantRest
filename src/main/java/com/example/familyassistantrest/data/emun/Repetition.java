65b22c0f1f5dd3b48db39fc3b0774f7ce3372104
Ilya Buyevich <ibuyevich@mdait.by>
2025-04-14 15:20:32 +0300

    Types of recurring events

---
+++ src/main/java/com/example/familyassistantrest/data/emun/Repetition.java
@@ -0,0 +1,12 @@
+package com.example.familyassistantrest.data.emun;
+
+/**
+ * Виды повторяющихся событий
+ *
+ * @author Buyevich_I
+ */
+public enum Repetition {
+    NOT_REPEAT(),//не повторять
+    GIVEN_QUANTITY(),//заданное количество раз
+    INFINITY(),// бесконечно
+}
